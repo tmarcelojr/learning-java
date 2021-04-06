7bbdcfb034753cb800c842c5e4ce94233d158e96
Teo Marcelo <tmarcelojr@gmail.com>
2021-04-05 23:59:46 -0500

    arrays

--- src/Hello.java
+++ src/Hello.java
@@ -1,12 +1,16 @@
+import java.util.Arrays;
+
 public class Hello {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
 
-        int myFirstNumber = (10 + 5) + (2 * 10);
-        int mySecondNumber = 12;
-        int myThirdNumber = 6;
-        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
-        System.out.println(myTotal);
+        String message = "Hello \"Teo\"";
+        // System.out.println(message);
+
+        //  Arrays
+        int[] numbers = {2, 3, 5, 1, 4};
+        Arrays.sort(numbers);
+        System.out.println(Arrays.toString(numbers));
     }
 }
