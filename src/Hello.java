cff1f6dbe43f9aaa3e925be9055198ec74eff4f4
Teo Marcelo <tmarcelojr@gmail.com>
2021-04-05 23:10:25 -0500

    worked with variables

--- src/Hello.java
+++ src/Hello.java
@@ -2,7 +2,11 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        System.out.println(myTotal);
     }
-
-
 }
