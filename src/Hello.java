404396135e9496d8679cfb052ed07c312c7e1658
Teo Marcelo <tmarcelojr@gmail.com>
2021-04-06 00:02:41 -0500

    created arrays using the curly syntax

--- src/Hello.java
+++ src/Hello.java
@@ -9,8 +9,11 @@ public class Hello {
         // System.out.println(message);
 
         //  Arrays
-        int[] numbers = {2, 3, 5, 1, 4};
-        Arrays.sort(numbers);
-        System.out.println(Arrays.toString(numbers));
+//        int[] numbers = {2, 3, 5, 1, 4};
+//        Arrays.sort(numbers);
+//        System.out.println(Arrays.toString(numbers));
+
+        int[][] numbers = { { 1, 2, 3 }, { 4, 5, 6} };
+        System.out.println(Arrays.deepToString(numbers));
     }
 }
